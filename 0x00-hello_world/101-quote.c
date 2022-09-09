dcfa668e1f00d3669f029b1a76c63fe4359b88f6
meenphilip <meenphilip@gmail.com>
2022-09-09 07:15:36 -0700

    refactor code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
+
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - main block
- * Description: Print text, followed
- * by a new line to the standard
- * error.
- * Return: 1
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, msg, sizeof(msg));
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
